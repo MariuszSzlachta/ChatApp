c6dcc9847d8d099d272ba6a9d1f95867d5e98617
Mariusz Szlachta <mariusz.szlachta@yahoo.com>
2018-07-27 14:15:04 +0200

    Add session basic integration

--- index.js
+++ index.js
@@ -8,10 +8,23 @@ const io = socketIo(server);
 const UserService = require('./UserService');
 
 const userService = new UserService();
+//for session storage
+
+const session = require("express-session")({
+  secret: "my-secret",
+  resave: true,
+  saveUninitialized: true
+});
+const sharedsession = require("express-socket.io-session");
+
+app.use(session);
+
+io.use(sharedsession(session));
 
 app.use(express.static(__dirname + '/public'));
 
-
+// ghdyby były przekierowania tutaj byłby może odczytywał cookie i od razu przekierowywał na czat gdzie ustawiałbym nazwe uzytkownia 
+// tyle ze jest to zapytanie GET a nie POST więc ja nic nie wysyłam do serwera prócz prośby o zwrócenie index.html
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 })
@@ -19,11 +32,31 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
   //funkcje wykonane po podłączeniu kleinta
   socket.on('join', function(name){
+    // sprawdzam czy nicka już nie ma w sesji jesli nie ma to tworze sesje i dodaje użytkownika do userService
+    if (socket.handshake.session.userdata !== name){
+      socket.handshake.session.userName = name;
+      socket.handshake.session.userSessionID = session.id;
+      socket.handshake.session.save();
+      userService.addUser({
+        id: socket.id,
+        name: name
+      });
+    } else {
+
+      //niby spoko ale powiela użytkownika tylko
+      userService.addUser({
+        id: socket.handshake.session.userSessionID,
+        name: socket.handshake.session.userName
+      });
+    }
+    console.log(userService.users)
+    //Musze przeszukać UserService.users czy jest tam name taki jak usedata jeśli jest to ustawiam nick taki jak z userservice a nie ten z name
+    
+
+    
+    
+
     //odczyt z LS a jak nie ma to addUser
-    userService.addUser({
-      id: socket.id,
-      name
-    });
     io.emit('update', {
       users: userService.getAllUsers()
     });
